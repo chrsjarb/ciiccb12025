3ef49cb9b77345bcfd797817d60cbea5a94851f7
chrsjarb <136284596+chrsjarb@users.noreply.github.com>
2025-05-30 15:10:53 +0000

    Add TaskSheet 1.1.1 - Print "Kamusta mundo!"

---
+++ TaskSheet/tasksheet_1_1_1.java
@@ -0,0 +1,23 @@
+package TaskSheet;
+
+class tasksheet_1_1_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");
+    }
+}
+
+
+
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+/* 
+clas helloWorldApp {
+    public static void main(String[] args) {
+        System.out.rintln("Hello World!); // Display the string.
+    }
+}
+*/
